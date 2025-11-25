4914a4794d14c55e250dfacbd86b14be15e7f47f
prince <prince9b407@gmail.com>
2025-11-25 16:41:31 +0530

    hathi

--- src/Develop.jsx
+++ src/Develop.jsx
@@ -4,6 +4,7 @@ const Develop = () => {
   return (
     <div>
       <h2>this is my  devlopbranch have added some critical feature in this branch </h2>
+      <h3>checking for merge and rebase in git and github </h3>
     </div>
   )
 }
