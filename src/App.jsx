e1eba69718eb675e1b3e8be31177962856b014ce
prince <prince9b407@gmail.com>
2025-11-25 12:28:03 +0530

    commit msg change in important

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Hello from './Hello'
 
 const App = () => {
   return (
     <div>
+      <Hello/>
       <h2>readme</h2>
       <h3>very improtant and complex thing have done ok</h3>
-      <h5> made changes in hundreds of file</h5>
+      <h5> made changes in hundreds of files</h5>
     </div>
   )
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+      <h4>hello</h4>
+    </div>
+  )
+}
+
+export default Hello
