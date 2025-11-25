0d70d37b088f551292024842165a8b73c7478e5e
prince <prince9b407@gmail.com>
2025-11-25 16:57:12 +0530

    stash

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,37 @@ import Hello from './Hello'
 
 const App = () => {
   return (
+<<<<<<< Updated upstream
     <div>
       <Hello/>
       <h2>readme</h2>
       <h3>very improtant and complex thing have done ok</h3>
       <h5> made changes in hundreds of files</h5>
     </div>
+=======
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+        hello
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+>>>>>>> Stashed changes
   )
 }
 
--- src/Develop.jsx
+++ src/Develop.jsx
@@ -5,6 +5,7 @@ const Develop = () => {
     <div>
       <h2>this is my  devlopbranch have added some critical feature in this branch </h2>
       <h3>checking for merge and rebase in git and github </h3>
+        <h5>isko stash karna hai</h5>
     </div>
   )
 }
